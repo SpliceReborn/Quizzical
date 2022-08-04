51d5b3a3d7c5ad99c35241e0118ce51052e4af9e
Darren <splicereborn@gmail.com>
2022-08-05 04:53:49 +0800

    Add initial loading screen, fix rerendering when restart

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './style.css'
 import Start from './Start'
 import Quiz from './Quiz'
-import {filterQuestionArray} from './utilities'
 
 export default function App() {
 
     const [gameStarted, setGameStarted] = useState(false)
-    const [gameOngoing, setGameOngoing] = useState(false)
-    const [questionArray, setQuestionArray] = useState([])
-
-    function checkOrRestart() {
-        setGameOngoing(prevGameOngoing => !prevGameOngoing)
-    }
+    const [initialLoad, setInitialLoad] = useState(false)
 
     function startGame() {
         setGameStarted(true)
-        setGameOngoing(true)
+        setInitialLoad(true)
+        setTimeout(() => {setInitialLoad(false)}, 900)
     }
-
-    useEffect(() => {
-        fetch("https://opentdb.com/api.php?amount=5")
-            .then(res => res.json())
-            .then(data => {
-                if (gameStarted && gameOngoing) setQuestionArray(filterQuestionArray(data.results))
-            })
-        
-    },[gameStarted, gameOngoing])
 
     return gameStarted ? 
             <main className="container">
                 <Quiz 
-                    questionArray={questionArray} 
-                    gameOngoing={gameOngoing}
-                    checkOrRestart={checkOrRestart}
+                    initialLoad={initialLoad}
                 />
             </main> : 
             <main className="container">
--- src/Quiz.js
+++ src/Quiz.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./style.css"
 import Question from './Question'
+import {filterQuestionArray} from './utilities'
 
 export default function Quiz(props) {
 
+    const [gameOngoing, setGameOngoing] = useState(true)
+    const [questionArray, setQuestionArray] = useState([])
     const [selectedOptions, setSelectedOptions] = useState({
         question1: "",
         question2: "",
@@ -11,27 +14,37 @@ export default function Quiz(props) {
         question4: "",
         question5: "",
     })
-
-    const correct_answers = []
-    props.questionArray.forEach(question => {
-        correct_answers.push(question.correct_answer)  
-    })
-
     const [score, setScore] = useState(0)
 
     function checkOrRestart() {
-        if (props.gameOngoing) {
-            correct_answers.forEach((ele, index) => {
-                if (selectedOptions[`question${index+1}`] === ele) {
+        if (gameOngoing) {
+            questionArray.forEach((ele, index) => {
+                if (selectedOptions[`question${index+1}`] === ele.correct_answer) {
                     setScore(prevScore => prevScore + 1)
                 }
             })
+            setGameOngoing(prevGameOngoing => !prevGameOngoing)
         } else {
-            setScore(0)
-        }
-
-        props.checkOrRestart()
+            fetch("https://opentdb.com/api.php?amount=5")
+                .then(res => res.json())
+                .then(data => {
+                    setQuestionArray(filterQuestionArray(data.results))
+                    setScore(0)
+                })
+        }       
     }    
+
+    useEffect(() => {
+        fetch("https://opentdb.com/api.php?amount=5")
+            .then(res => res.json())
+            .then(data => {
+                setQuestionArray(filterQuestionArray(data.results))
+            })
+    }, [])
+
+    useEffect(() => {
+        setGameOngoing(prevGameOngoing => !prevGameOngoing)
+    }, [questionArray])
 
     function handleChange(event, questionNumber) {
         const {value} = event.target
@@ -41,7 +54,7 @@ export default function Quiz(props) {
         }))
     }
 
-    const questions = props.questionArray.map(({id, question, correct_answer, options}, index) => { 
+    const questions = questionArray.map(({id, question, correct_answer, options}, index) => { 
         return (
             <Question 
                 key={id} 
@@ -50,15 +63,15 @@ export default function Quiz(props) {
                 options={options}
                 answer={correct_answer}
                 handleChange={handleChange}
-                gameOngoing={props.gameOngoing}
+                gameOngoing={gameOngoing}
             />
         )
     })
 
     return (
         <div className="question-page">
-            {questions}
-            {props.gameOngoing ? 
+            {props.initialLoad ? <h1>Loading ... </h1> : questions}
+            {props.initialLoad ? <></> : gameOngoing ? 
                 <button className="button" onClick={checkOrRestart}>Check answers</button> : 
                 <div className="scoreContainer">
                     <p>You scored {score}/5 correct answers</p>
